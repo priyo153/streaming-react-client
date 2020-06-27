e4da3bbd528dd8b8cfb3157599080ef5d654f21a
priyo153 <priyojit.chatterjee333@gmail.com>
2020-06-28 01:33:56 +0530

    saturday updates

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,12 +1,3 @@
-// export const SIGN_IN = "SIGN_IN";
-// export const SIGN_OUT = "SIGN_OUT";
-// export const CREATE_STREAM = "CREATE_STREAM";
-// export const FETCH_STREAM = "FETCH_STREAM";
-// export const FETCH_STREAMS = "FETCH_STREAMS";
-// export const DELETE_STREAM = "DELETE_STREAM";
-// export const EDIT_STREAM = "EDIT_STREAM";
-
-//-------------------------------------------
 export const CREATE_STORE = "CREATE_STORE";
 export const CREATE_ORDER = "CREATE_ORDER";
 
